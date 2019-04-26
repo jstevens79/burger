b6ef7c28d16dba4b748b22d975735d3dcdb013ae
jstevens79 <jstevens79@users.noreply.github.com>
2019-04-26 18:21:16 -0500

    setting up files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+app.listen(PORT, () => {
+  console.log("Server started")
+})
